6ee5d60f2b86835fc5364d7d6363e0eea1928ac7
JungEun Park <jungeunp97@naver.com>
2025-05-30 09:47:34 +0900

    [test] : 로그인 API 테스트 작성 완료

--- sprint-mission-5/src/routers/usersRouter.test.ts
+++ sprint-mission-5/src/routers/usersRouter.test.ts
@@ -2,18 +2,12 @@ import app from '../app';
 import request from 'supertest';
 import { prismaClient } from '../lib/prismaClient';
 import { clearTestDB, createTestUser } from '../lib/testUtils';
-
-// 더미 데이터
-const testUser1 = {
-  email: 'test@example.com',
-  nickname: 'testUser1',
-  password: 'password1234',
-};
+import { User } from '../typings/userTypes';
 
 describe('로그인 테스트', () => {
   beforeEach(async () => {
     await clearTestDB();
-    await createTestUser(testUser1);
+    await createTestUser(1);
   });
   afterAll(async () => {
     await prismaClient.$disconnect();
@@ -21,7 +15,7 @@ describe('로그인 테스트', () => {
   test('POST /users/login', async () => {
     const user = await request(app)
       .post('/users/login')
-      .send({ email: testUser1.email, password: testUser1.password });
+      .send({ email: `testuser1@example.com`, password: 'password1234' });
     expect(user.status).toBe(200);
     expect(user.body.message).toBe('Log In Success');
     expect(user.headers['set-cookie']).toBeTruthy();
