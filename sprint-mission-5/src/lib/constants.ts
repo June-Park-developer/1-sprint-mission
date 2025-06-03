fdb5e5eea0e6132267d195c79a153c07a1e9771b
JungEun Park <jungeunp97@naver.com>
2025-06-03 10:55:58 +0900

    [feat] : s3 기능을 추가하였음..

--- sprint-mission-5/src/lib/constants.ts
+++ sprint-mission-5/src/lib/constants.ts
@@ -6,3 +6,7 @@ export const PORT = process.env.PORT || 3000;
 export const PUBLIC_PATH = './public';
 export const STATIC_PATH = '/public';
 export const JWT_SECRET = process.env.JWT_SECRET;
+export const AWS_REGION = process.env.AWS_REGION;
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
